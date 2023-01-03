3b987283cb5a6008ec850992b9155f9c8aae523a
Pietrodo-Code <pietrodoh71@gmail.com>
2023-01-03 09:20:02 -0300

    like

--- heart.js
+++ heart.js
@@ -1,9 +1,35 @@
-const btn_like = document.getElementById("btn_heart");
+//Botão01
+const btn_like01 = document.getElementById("btn_heart01");
+function liked01(){
 
-function liked(){
-
-    const like = document.getElementById("btn_heart");
+    const like = document.getElementById("btn_heart01");
     like.classList.toggle('active')
 }
+btn_like01.addEventListener("click",liked01);
 
-btn_like.addEventListener("click",liked);
+//Botão02
+const btn_like02 = document.getElementById("btn_heart02");
+function liked02(){
+
+    const like = document.getElementById("btn_heart02");
+    like.classList.toggle('active')
+}
+btn_like02.addEventListener("click",liked02);
+
+//Botão03
+const btn_like03 = document.getElementById("btn_heart03");
+function liked03(){
+
+    const like = document.getElementById("btn_heart03");
+    like.classList.toggle('active')
+}
+btn_like03.addEventListener("click",liked03);
+
+//Botão04
+const btn_like04 = document.getElementById("btn_heart04");
+function liked04(){
+
+    const like = document.getElementById("btn_heart04");
+    like.classList.toggle('active')
+}
+btn_like04.addEventListener("click",liked04);
